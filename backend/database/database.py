84ac1554f3153164b0c490decad6a4424dd29805
Артем <n1zel1x@MacBook-Pro-Artem.local>
2025-10-29 15:27:25 +0500

    first commit

--- backend/database/database.py
+++ backend/database/database.py
@@ -3,14 +3,22 @@ from sqlalchemy.orm import DeclarativeBase
 from typing import Annotated
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from fastapi import Depends
+from dotenv import load_dotenv
+
+from database.hash import config
 
 env = Env()
 
-DATABASE_URL = env.str('DATABASE_URL', default='postgresql+asyncpg://postgres:postgres@localhost:5432/todouserdb')
+load_dotenv()
+
+DATABASE_URL = env.str('DATABASE_URL')
 
 engine = create_async_engine(DATABASE_URL, future=True, echo=False)
 
 new_session = async_sessionmaker(autoflush=False, expire_on_commit=False, bind=engine)
+
+SECRET_KEY = env.str("SECRET_KEY")
+config.JWT_SECRET_KEY = SECRET_KEY
 
 #Some functions
 async def get_session():

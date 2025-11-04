dbfb3a9a68566a56036b07e13456c43b2eb49570
Артем <n1zel1x@MacBook-Pro-Artem.local>
2025-11-04 16:43:58 +0500

    Adapt frontend for phones

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,10 @@ from backend.database.database import engine, Base
 app = FastAPI()
 
 #-------------For render-------------
-@app.on_event("startup")
-async def on_startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+#@app.on_event("startup")
+#async def on_startup():
+#    async with engine.begin() as conn:
+#        await conn.run_sync(Base.metadata.create_all)
 #-------------------------------------
 
 app.add_middleware(
